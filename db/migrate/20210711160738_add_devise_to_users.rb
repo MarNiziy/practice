d78c25c4c065ea519e5acd5476bb9cba8c1e478b
Марина Низий <43046245+MarNiziy@users.noreply.github.com>
2021-07-16 11:56:58 +0300

    Update 20210711160738_add_devise_to_users.rb

--- db/migrate/20210711160738_add_devise_to_users.rb
+++ db/migrate/20210711160738_add_devise_to_users.rb
@@ -5,7 +5,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
     change_table :users do |t|
       ## Database authenticatable
       #t.string :name,              null: false, default: ""
-      #t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
