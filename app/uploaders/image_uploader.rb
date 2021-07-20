c4ab02475e89eac4627cd4d0821b9cff878a2604
MarNiziy <mary.nizy@yandex.ru>
2021-07-20 15:46:55 +0300

    general changes

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,10 +1,14 @@
 class ImageUploader < Shrine
 	plugin :cached_attachment_data
 	plugin :default_url
+	plugin :validation_helpers
+
+	Attacher.validate do
+    	validate_extension %w[jpg jpeg png webp]
+  	end
 
 	Attacher.default_url do |**options|
- 	  "/placeholders/missing.jpg"
+ 	  	"/placeholders/missing.jpg"
     end
-  # plugins and uploading logic
 end
 
