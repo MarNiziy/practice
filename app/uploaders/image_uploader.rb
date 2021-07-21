b322fea2506eb9439ce1176c2e685f95aae59780
MarNiziy <mary.nizy@yandex.ru>
2021-07-21 14:10:15 +0300

    General changes

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,7 @@ class ImageUploader < Shrine
 	plugin :cached_attachment_data
 	plugin :default_url
 	plugin :validation_helpers
+	plugin :determine_mime_type
 
 	Attacher.validate do
     	validate_extension %w[jpg jpeg png webp]
