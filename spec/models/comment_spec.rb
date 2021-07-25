f797bbcbf761219cd0ab2ad56e4c0502125ca341
MarNiziy <mary.nizy@yandex.ru>
2021-07-25 15:44:42 +0300

    added a validation test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe Comment, type: :model do
 
 	it{is_expected.to belong_to(:post)}
 	it{is_expected.to belong_to(:user)}
+
+	it{is_expected.to validate_presence_of(:content)}
+	it{is_expected.to validate_length_of(:content).is_at_most(200)}
 end
